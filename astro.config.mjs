48bd9d0ec09f417ef3c19ba217be39b5f38d9e95
Link <1316341442@qq.com>
2026-08-19 13:31:02 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,23 +15,7 @@ export default defineConfig({
     inspectorPort: isBuild ? false : undefined,
   }),
   security: {
-    checkOrigin: false,
-    csp: {
-      directives: [
-        "default-src 'self'",
-        "media-src 'self' https://files.atlinker.cn",
-        "img-src 'self' data: blob: https://files.atlinker.cn",
-        "connect-src 'self' https://static.cloudflareinsights.com",
-        "font-src 'self' data:",
-      ],
-      scriptDirective: {
-        src: ["'self'", "https://static.cloudflareinsights.com"],
-      },
-      styleDirective: {
-        src: ["'self'"],
-        unsafeInline: true, // live.js 动态设置内联样式必需
-      },
-    },
+    checkOrigin: true,
   },
   integrations: [themeLoader(), pluginLoader(), clientLoader()],
   vite: {
